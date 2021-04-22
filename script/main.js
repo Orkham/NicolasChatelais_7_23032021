2c7ecbc08b295fdd3392777b0ecfd9cdf4c57fd7
Nicolas Chatelais <ni.chatelais@gmail.com>
2021-04-22 17:27:39 +0200

    display searchbar

--- script/main.js
+++ script/main.js
@@ -1,14 +1,5 @@
 import {recipes} from './recipes.js'
 
-/*
-fetch('recipes.json')
-.then(function(response){
-    return response.json();
-})
-.then (response=>console.log(response))
-*/
-
-
 /*** Parcourir les recettes et extraire les ingrédients ***/
 
 let ingredientsListToDisplay = [];
@@ -24,7 +15,6 @@ for(let i = 0 ; i < recipes.length ; i++){
     }
     
 }
-//console.log(ingredientsListToDisplay)
 
 /*** Affichage des ingrédients ***/
 let ingredientsBox = document.getElementById('ingredientsBox')
@@ -33,11 +23,37 @@ function displayIngredient(){
         let ingredientToDisplay = document.createElement("li");
         ingredientToDisplay.appendChild(document.createTextNode(ingredientsListToDisplay[i]));
         ingredientsBox.appendChild(ingredientToDisplay);
-        ingredientToDisplay.className = "col-4 ingredientInSearchResult";
+        ingredientToDisplay.className = "col-4";
     }
     
 }
 displayIngredient();
+
+/*** Parcourir les recettes et extraire les appareils ***/
+
+let appareilsListToDisplay = [];
+
+for(let i = 0 ; i < recipes.length ; i++){
+    
+    if(!appareilsListToDisplay.includes(recipes[i].appliance)){
+        appareilsListToDisplay.push(recipes[i].appliance)
+    }
+    
+}
+console.log(appareilsListToDisplay)
+/*** Affichage des appareils ***/
+let appareilsBox = document.getElementById('appareilsBox')
+function displayAppareils(){
+    for(let i = 0 ; i < appareilsListToDisplay.length ; i ++){
+        let appareilToDisplay = document.createElement("li");
+        appareilToDisplay.appendChild(document.createTextNode(appareilsListToDisplay[i]));
+        appareilsBox.appendChild(appareilToDisplay);
+        appareilToDisplay.className = "col-4";
+    }
+    
+}
+displayAppareils();
+
 /***Ecouteurs dans la liste des ingrédients ***/
 const tagsListBox = document.getElementById('tagsList')
 for(let i = 0 ; i < ingredientsBox.childElementCount ; i++){
@@ -58,12 +74,6 @@ for(let i = 0 ; i < ingredientsBox.childElementCount ; i++){
 }
 /*****Cancel Cross *****/
 let crossArray = document.getElementsByClassName('cancelCross');
-
-/*
-function removeSearchResult(resultToRemove){
-    resultToRemove.remove();
-}
-*/
 
 for(let cross of crossArray){
     cross.addEventListener('click', (e)=>e.target.parentElement.remove())
@@ -141,6 +151,7 @@ searchByAppareil.addEventListener('input', displaySearchByAppareilResults);
 searchByUstensiles.addEventListener('input', displaySearchByUstensilesResults);
 
 /*****Selects*****/
+/*Ingredients*/
 let ingredientsList = document.getElementById('ingredientsList');
 let ingredientsSearchbarContainer = document.getElementById('ingredientsSearchbarContainer');
 let chevrondownIngredients = document.getElementById('chevron-downIngredients');
@@ -156,87 +167,124 @@ function displayIngredientsList(){
     chevrondownIngredients.classList.toggle('d-none');
     chevronupIngredients.classList.toggle('d-none');
 }
+/*Appareil*/
+let appareilsList = document.getElementById('appareilsList');
+let appareilsSearchbarContainer = document.getElementById('appareilsSearchbarContainer');
+let chevrondownAppareils = document.getElementById('chevron-downAppareil');
+let chevronupAppareils = document.getElementById('chevron-upAppareil');
+
+searchByAppareil.addEventListener('click', displayAppareilsList);
+
+function displayAppareilsList(){
+    appareilsList.classList.toggle('d-none');
+    appareilsList.classList.toggle('width-xl');
+    searchByAppareil.classList.toggle('width-xl');
+    appareilsSearchbarContainer.classList.toggle('width-xl');
+    chevrondownAppareils.classList.toggle('d-none');
+    chevronupAppareils.classList.toggle('d-none');
+}
+
+
+
+
+
 
 /***Affichage des recettes***/
 
-console.log(recipes)
 let recipesBoxContainer = document.getElementById('recipesBoxContainer')
 
-
 function createRecipeDisplay(){
-    let newCard = document.createElement("div");
-    newCard.className = "card mb-5";
-    newCard.innerHTML = '<svg class="bd-placeholder-img card-img-top" width="100%" height="178" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#C7BEBE"/><text x="50%" y="50%" fill="#eceeef" dy=".3em"></text></svg>';
+    //console.log(recipes)
+    for(let i = 0 ; i < 6 ; i++){
+        let newCard = document.createElement("div");
+        newCard.className = "card mb-5";
+        newCard.innerHTML = '<svg class="bd-placeholder-img card-img-top" width="100%" height="178" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#C7BEBE"/><text x="50%" y="50%" fill="#eceeef" dy=".3em"></text></svg>';
 
-    let newCardBody = document.createElement("div");
-    newCardBody.className = "card-body description pb-5"
-    newCard.appendChild(newCardBody)
+        let newCardBody = document.createElement("div");
+        newCardBody.className = "card-body description pb-5"
+        newCard.appendChild(newCardBody)
 
-    let newRecipeTitle = document.createElement("div");
-    newRecipeTitle.className = "recipesTitle d-flex justify-content-between pr-0 align-items-center mb-3"
-    newCardBody.appendChild(newRecipeTitle);
+        let newRecipeTitle = document.createElement("div");
+        newRecipeTitle.className = "recipesTitle d-flex justify-content-between pr-0 align-items-center mb-3"
+        newCardBody.appendChild(newRecipeTitle);
 
-    let newCardTitle = document.createElement("h2");
-    newCardTitle.className = "card-title  mb-0";
-    /**/
-    newCardTitle.appendChild(document.createTextNode("Lait de Coco"))
-    /**/
-    newRecipeTitle.appendChild(newCardTitle);
+        let newCardTitle = document.createElement("h2");
+        newCardTitle.className = "card-title  mb-0";
+        newCardTitle.appendChild(document.createTextNode(recipes[i].name))
+        newRecipeTitle.appendChild(newCardTitle);
 
-    let newTimingDiv = document.createElement("div");
-    newTimingDiv.className = "timing d-flex align-items-center  mb-0";
-    newRecipeTitle.appendChild(newTimingDiv);
+        let newTimingDiv = document.createElement("div");
+        newTimingDiv.className = "timing d-flex align-items-center  mb-0";
+        newRecipeTitle.appendChild(newTimingDiv);
 
-    let newClockIcon = document.createElement("i");
-    newClockIcon.className = "far fa-clock pr-1 d-flex align-items-center mb-0"
-    newTimingDiv.appendChild(newClockIcon)
-    let newTimingDuration = document.createElement("p");
-    newTimingDuration.className = "d-flex align-items-center mb-0 timing__duration";
-    /**/
-    newTimingDuration.appendChild(document.createTextNode("10 min"))
-    /**/
-    newTimingDiv.appendChild(newTimingDuration);
+        let newClockIcon = document.createElement("i");
+        newClockIcon.className = "far fa-clock pr-1 d-flex align-items-center mb-0"
+        newTimingDiv.appendChild(newClockIcon)
+        let newTimingDuration = document.createElement("p");
+        newTimingDuration.className = "d-flex align-items-center mb-0 timing__duration";
+        newTimingDuration.appendChild(document.createTextNode(recipes[i].time + " min"))
+        newTimingDiv.appendChild(newTimingDuration);
 
-    let flexDiv = document.createElement("div");
-    flexDiv.className = "d-flex p-0";
-    newCardBody.appendChild(flexDiv)
+        let flexDiv = document.createElement("div");
+        flexDiv.className = "d-flex p-0";
+        newCardBody.appendChild(flexDiv)
 
-    let newUl = document.createElement("ul");
-    newUl.className = "list-group pr-3 w-50";
-    flexDiv.appendChild(newUl);
+        let newUl = document.createElement("ul");
+        newUl.className = "list-group pr-3 w-50";
+        flexDiv.appendChild(newUl);
 
-    let newLi = document.createElement("li");
-    newLi.className = "list-group-item ingredient d-flex p-0";
-    newUl.appendChild(newLi);
-    
+        for(let j = 0 ; j < recipes[i].ingredients.length ; j++){
+            let newLi = document.createElement("li");
+            newLi.className = "list-group-item ingredient d-flex p-0";
+            newUl.appendChild(newLi);
+        
 
-    /***Boucle pour les ingrédients ***/
+        /***Boucle pour les ingrédients ***/
+        
+            let newIngredient = document.createElement("p");
+            newIngredient.className = "mb-0 font-weight-bold"
+            newIngredient.appendChild(document.createTextNode(recipes[i].ingredients[j].ingredient))
+            newLi.appendChild(newIngredient);
 
-    let newIngredient = document.createElement("p");
-    newIngredient.className = "mb-0 font-weight-bold"
-    /**/
-    newIngredient.appendChild(document.createTextNode("Lait de coco:"))
-    /**/
-    newLi.appendChild(newIngredient);
+            let newQuantity = document.createElement("p");
+            newQuantity.className = "mb-0 pl-1";
+            
+            if(recipes[i].ingredients[j].quantity){
+                newQuantity.appendChild(document.createTextNode(" : " + recipes[i].ingredients[j].quantity + " " + displayUnit(recipes[i].ingredients[j].unit)))
+            }
+            
+            newLi.appendChild(newQuantity);
+        }
 
-    let newQuantity = document.createElement("p");
-    newQuantity.className = "mb-0 pl-1";
-    /**/
-    newQuantity.appendChild(document.createTextNode("400ml"))
-    /**/
-    newLi.appendChild(newQuantity);
-
-/******************************/
-
-    let newInstructions = document.createElement("p");
-    newInstructions.className = "instructions w-50"
-    /**/
-    newInstructions.appendChild(document.createTextNode("Mettre les glaçons à votre goût dans le blender, ajouter le lait, la crème de coco, le jus de 2 citrons et le sucre. Mixer jusqu'à avoir la consistence désirée."))
-    /**/
-    flexDiv.appendChild(newInstructions);
-
+        let newInstructions = document.createElement("p");
+        newInstructions.className = "instructions w-50"
+        
+        let textDescription = recipes[i].description.substring(0,2000)
+        newInstructions.appendChild(document.createTextNode(textDescription))
+        
+        flexDiv.appendChild(newInstructions);
+        
+        //console.log(newInstructions.textContent.substring(0,100))
 
 
-    recipesBoxContainer.appendChild(newCard)
+
+        recipesBoxContainer.appendChild(newCard)
+    }
 }
 createRecipeDisplay()
+
+function displayUnit(unit){
+    if(unit === "grammes"){
+        return 'g'
+    }else if(unit){
+        return unit
+    }else{
+        return ""
+    }
+}
+
+function isTextTooLong(){
+    if(text.length > 200){
+
+    }
+}
